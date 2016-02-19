0a3b5e3d2126358e2f2498794edacac4f8fc2417
Anil Madhavapeddy <anil@recoil.org>
2016-02-19 16:35:21 +0000

    Use <endian.h> instead of <sys/endian.h>
    Fixes compilation on musl-libc based distributions such as Alpine
    Linux, and continues to work on Ubuntu.

--- src/bigstring_stubs.c
+++ src/bigstring_stubs.c
@@ -33,7 +33,7 @@
 #include <endian.h>
 #else
 #include <sys/types.h>
-#include <sys/endian.h>
+#include <endian.h>
 #define __BYTE_ORDER    _BYTE_ORDER
 #define __LITTLE_ENDIAN _LITTLE_ENDIAN
 #define __BIG_ENDIAN    _BIG_ENDIAN
