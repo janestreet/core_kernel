634d635ca6a2e388cab841e8b645f979edfb96b6
Jeremie Dimino <jdimino@janestreet.com>
2016-08-31 11:54:02 +0100

    114.08+33

--- include/core_params.h
+++
@@ -1,7 +0,0 @@
-#ifndef PARAMS_H
-#define PARAMS_H
-
-/* I/O transaction size after which to release the OCaml-lock */
-#define THREAD_IO_CUTOFF 65536
-
-#endif /* PARAMS_H */
--- install.ml
+++ install.ml
@@ -6,8 +6,5 @@ open Oasis2opam_install;;
 generate ~package:"core_kernel"
   [ oasis_lib "core_kernel"
   ; file "META" ~section:"lib"
-  ; file "include/core_params.h" ~section:"lib"
-  ; file "include/jane_common.h" ~section:"lib"
-  ; file "include/ocaml_utils.h" ~section:"lib"
   ; file "src/time_ns_stubs.h" ~section:"lib"
   ]
