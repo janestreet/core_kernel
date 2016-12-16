0866003731dfdb72b415faf4480f7a522cc07bff
Jeremie Dimino <jdimino@janestreet.com>
2016-12-16 11:36:00 +0000

    Don't pass -lrt when not available
    Closes #61

--- src/rt-flags.sh
+++ src/rt-flags.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-# This file is overridden in the public release as some system don't
-# have -lrt
-echo "(rt)"
+if ld -lrt -shared -o /dev/null 2>/dev/null; then
+    echo "(rt)"
+else
+    echo "()"
+fi
