56fa2f1887f4b09fa55d153b2314596c0a6ab8d8
Xavier Clerc <xclerc@janestreet.com>
2019-04-15 14:50:55 +0100

    v0.13-preview.120.31+227

--- src/hash_queue_intf.ml
+++ src/hash_queue_intf.ml
@@ -157,3 +157,22 @@ module type S = sig
 
   val foldi : 'a t -> init:'b -> f:('b -> key:Key.t -> data:'a -> 'b) -> 'b
 end
+
+(** A hash-queue is a combination of a queue and a hashtable that
+    supports constant-time lookup and removal of queue elements in addition to
+    the usual queue operations (enqueue, dequeue). The queue elements are
+    key-value pairs. The hashtable has one entry for each element of the queue.
+
+    Calls to functions that would modify a hash-queue (e.g. [enqueue], [dequeue],
+    [remove], [replace]) detect if a client is in the middle of iterating over the
+    queue (e.g., [iter], [fold], [for_all], [exists]) and if so, raise an exception.
+*)
+module type Hash_queue = sig
+  module type Key = Key
+  module type S = S
+
+  module Make_with_table (Key : Key) (Table : Hashtbl_intf.S_plain with type key = Key.t) :
+    S with module Key = Key
+
+  module Make (Key : Key) : S with module Key = Key
+end
