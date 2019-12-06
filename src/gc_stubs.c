26b1a5ee92687fd35b361920e75fd031e4eccd9c
Xavier Clerc <xclerc@janestreet.com>
2019-12-06 13:25:06 +0000

    Tentative patch for OCaml 4.10 support.

--- src/gc_stubs.c
+++ src/gc_stubs.c
@@ -1,4 +1,24 @@
 #include <caml/memory.h>
+#include <caml/version.h>
+
+#if OCAML_VERSION >= 41000
+#define caml_stat_minor_words Caml_state_field(stat_minor_words)
+#define caml_stat_promoted_words Caml_state_field(stat_promoted_words)
+#define caml_stat_major_words Caml_state_field(stat_major_words)
+
+extern uintnat caml_allocated_words;
+
+#define caml_stat_minor_collections Caml_state_field(stat_minor_collections)
+#define caml_stat_major_collections Caml_state_field(stat_major_collections)
+#define caml_stat_heap_wsz Caml_state_field(stat_heap_wsz)
+#define caml_stat_top_heap_wsz Caml_state_field(stat_top_heap_wsz)
+#define caml_stat_compactions Caml_state_field(stat_compactions)
+#define caml_stat_heap_chunks Caml_state_field(stat_heap_chunks)
+
+#define caml_stat_minor_words Caml_state_field(stat_minor_words)
+#define caml_stat_major_words Caml_state_field(stat_major_words)
+
+#else
 
 extern double caml_stat_minor_words;
 extern double caml_stat_promoted_words;
@@ -15,6 +35,7 @@ extern intnat caml_stat_heap_wsz;
 extern intnat caml_stat_top_heap_wsz;
 extern intnat caml_stat_compactions;
 extern intnat caml_stat_heap_chunks;
+#endif
 
 static intnat minor_words(void)
 {
