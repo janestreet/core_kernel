5b354e77a72bde7e316a327bfc8c78a9df6a9b6d
Florian Angeletti <florian.angeletti@inria.fr>
2020-01-17 11:11:54 +0100

    Alternative 4.10.0+beta2 patch

--- src/gc_stubs.c
+++ src/gc_stubs.c
@@ -1,41 +1,7 @@
+#define CAML_NAME_SPACE
+#define CAML_INTERNALS
 #include <caml/memory.h>
-#include <caml/version.h>
-
-#if OCAML_VERSION >= 41000
-#define caml_stat_minor_words Caml_state_field(stat_minor_words)
-#define caml_stat_promoted_words Caml_state_field(stat_promoted_words)
-#define caml_stat_major_words Caml_state_field(stat_major_words)
-
-extern uintnat caml_allocated_words;
-
-#define caml_stat_minor_collections Caml_state_field(stat_minor_collections)
-#define caml_stat_major_collections Caml_state_field(stat_major_collections)
-#define caml_stat_heap_wsz Caml_state_field(stat_heap_wsz)
-#define caml_stat_top_heap_wsz Caml_state_field(stat_top_heap_wsz)
-#define caml_stat_compactions Caml_state_field(stat_compactions)
-#define caml_stat_heap_chunks Caml_state_field(stat_heap_chunks)
-
-#define caml_stat_minor_words Caml_state_field(stat_minor_words)
-#define caml_stat_major_words Caml_state_field(stat_major_words)
-
-#else
-
-extern double caml_stat_minor_words;
-extern double caml_stat_promoted_words;
-extern double caml_stat_major_words;
-
-extern char *caml_young_ptr;
-extern char *caml_young_end;
-
-extern uintnat caml_allocated_words;
-
-extern intnat caml_stat_minor_collections;
-extern intnat caml_stat_major_collections;
-extern intnat caml_stat_heap_wsz;
-extern intnat caml_stat_top_heap_wsz;
-extern intnat caml_stat_compactions;
-extern intnat caml_stat_heap_chunks;
-#endif
+#include <caml/gc_ctrl.h>
 
 static intnat minor_words(void)
 {
