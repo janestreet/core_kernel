8edc7a234d870d69bea97b6feeda4e573efc8433
Kate <kit.ty.kate@disroot.org>
2020-11-05 00:28:15 +0000

    Add support for OCaml 4.12
    Signed-off-by: Kate <kit.ty.kate@disroot.org>

--- src/gc.ml
+++ src/gc.ml
@@ -16,6 +16,31 @@ include Caml.Gc
 
 module Stat = struct
   module T = struct
+    [%%if ocaml_version >= (4, 12, 0)]
+
+    type t = Caml.Gc.stat =
+      { minor_words : float
+      ; promoted_words : float
+      ; major_words : float
+      ; minor_collections : int
+      ; major_collections : int
+      ; heap_words : int
+      ; heap_chunks : int
+      ; live_words : int
+      ; live_blocks : int
+      ; free_words : int
+      ; free_blocks : int
+      ; largest_free : int
+      ; fragments : int
+      ; compactions : int
+      ; top_heap_words : int
+      ; stack_size : int
+      ; forced_major_collections : int
+      }
+    [@@deriving compare, hash, bin_io, sexp, fields]
+
+    [%%else]
+
     type t = Caml.Gc.stat =
       { minor_words : float
       ; promoted_words : float
@@ -35,7 +60,9 @@ module Stat = struct
       ; stack_size : int
       }
     [@@deriving compare, hash, bin_io, sexp, fields]
-  end
+
+    [%%endif]
+   end
 
   include T
   include Comparable.Make (T)
